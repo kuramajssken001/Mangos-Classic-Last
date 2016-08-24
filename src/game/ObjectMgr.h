a5c3493c53b39bacd5245708f10c5953981b832f
kuramajssken001 <kuramajssken1234567@gmail.com>
2016-08-24 11:39:48 +0800

    Fix npc texts the database

--- src/game/ObjectMgr.h
+++ src/game/ObjectMgr.h
@@ -96,7 +96,7 @@ typedef std::unordered_map < uint32/*mapid*/, CellObjectGuidsMap > MapObjectGuid
 #define MIN_DB_SCRIPT_STRING_ID        MAX_MANGOS_STRING_ID // 'db_script_string'
 #define MAX_DB_SCRIPT_STRING_ID        2001000000
 #define MIN_CREATURE_AI_TEXT_STRING_ID (-1)                 // 'creature_ai_texts'
-#define MAX_CREATURE_AI_TEXT_STRING_ID (-1000000)
+#define MAX_CREATURE_AI_TEXT_STRING_ID (-100000000)
 // Anything below MAX_CREATURE_AI_TEXT_STRING_ID is handled by the external script lib
 
 static_assert(MAX_DB_SCRIPT_STRING_ID < INT_MAX, "Must scope with int32 range");
